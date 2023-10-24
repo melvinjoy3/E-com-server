a1d190108d0c6e20edfc28bae0e372d33abc7476
melvinjoy3 <melvinjoy8@gmail.com>
2023-10-24 22:06:07 +0530

    dotenv added

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const MongoClient = require("mongodb").MongoClient;
+const dontenv = require("dotenv");
+dontenv.config();
 
 const mongoURI = "mongodb://localhost:27017/";
-
+// mongodb+srv://melvinjoy3:${process.env.CLUSTER_PASSWORD}@ecom-cluster.yxzcttr.mongodb.net/
 module.exports.connect = async function () {
   const client = await MongoClient.connect(mongoURI, {
     useNewUrlParser: true,
@@ -9,8 +11,6 @@ module.exports.connect = async function () {
   return client;
 };
 
-
 module.exports.get = function () {
   return state.db;
 };
-
